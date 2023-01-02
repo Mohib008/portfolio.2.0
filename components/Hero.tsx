0b5af29fd10b72507879f1b2c27efca6a4b756a1
Mohib <mohib.0008@gmail.com>
2023-01-02 14:39:00 -0500

    new pics

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
 import BackgroundCircles from './BackgroundCircles';
+import { mohib } from "../public/mohib.jpg";
+
 
 type Props = {}
 
 function Hero({}: Props) {
     const [text, count] = useTypewriter({
         words: [
-            "Hi, The Name's Mohib Arsala",
-            "Guy who love to learn more about JS & React.tsx",
-            "I Love to code everyday & learn about all languages",
+            "Hi, This is Mohib Arsala",
+            "Guys-who-love-NextJs/tsx",
+            "<ButLoveToCodeMore/>",
         ],
         loop: true,
         delaySpeed: 2000,
@@ -17,12 +19,29 @@ function Hero({}: Props) {
 
 
   return (
-    <div>
+    <div className='h-screen flex flex-col overflow-hidden text-center justify-center 
+    space-y-8 items-center'>
         <BackgroundCircles />
-        <h1>
-            <span>{text}</span>
-            <Cursor cursorColor="#F7AB0A" />
-        </h1>
+        <img 
+         src="mohib.jpg" 
+         alt=""
+         className='relative rounded-full h-32 w-32 mx-auto object-cover'
+         />
+
+        <div>
+          <h2 className='text-sm text-gray-500 uppercase tracking-[15px]'>
+            Software Engineer
+          </h2>
+          <h1 className='text-5xl lg:6xl text-samibold text-gray-500'>
+              <span>{text}</span>
+              <Cursor cursorColor="#F7AB0A" />
+          </h1>
+          <div className="div" />
+          <button className="heroButton">About</button>
+          <button className="heroButton">Experience</button>
+          <button className="heroButton">Skills</button>
+          <button className="heroButton">Projects</button>
+        </div>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,16 @@ import Hero from '../components/Hero'
 
 const Home: NextPage = () => {
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen'>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
         <title>Mohib's Portfolio</title>
       </Head>
      
      <Header />
+     <section id='hero' className='snap-center'>
+       <Hero />
+     </section>
 
-     <Hero />
 
      {/*about*/}
 
